40527ddd45f5adbc063e2fadf68d6a6d47dd31a6
Rhunwolf <dm15r@yandex.ru>
2017-01-20 15:12:52 +0300

    hw

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(3, 1);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Hello world");
+            Console.SetCursorPosition(3, 2);
+            Console.ReadLine();
+        }
+    }
+}
